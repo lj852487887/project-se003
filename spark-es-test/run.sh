c38c999654adcb7b24dbafc7f3d3df0d44660312
lj852487887 <1007913793@qq.com>
2016-12-10 21:39:45 +0800

    tested spark-es

--- spark-es-test/run.sh
+++ spark-es-test/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo "compile myFirstScalaApp"
+echo "compile spark-es test"
 sbt clean package
-echo "run wordcount on the cluster"
+echo "run spark-es test local"
 
-/opt/spark/bin/spark-submit --class SparkES ./target/scala-2.10/sparkes-test_2.10-1.0.jar
+/opt/spark/bin/spark-submit --class SparkES --jars ./elasticsearch-spark_2.10-2.2.0.jar ./target/scala-2.10/sparkes-test_2.10-1.0.jar
 
 
 
