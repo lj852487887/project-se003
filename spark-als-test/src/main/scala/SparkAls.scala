4ee92bb648c953de18234e575bd6cbf2756553e6
李骏 <lj852487887@gmail.com>
2016-12-11 17:08:39 +0800

    update

--- spark-als-test/src/main/scala/SparkAls.scala
+++ spark-als-test/src/main/scala/SparkAls.scala
@@ -6,26 +6,27 @@ import org.apache.spark.{SparkConf, SparkContext}
 
 import org.elasticsearch.spark.sql._
 
+import org.apache.spark.mllib.recommendation.ALS
+
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
 
-object SparkES {
+object SparkAls {
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
 
-    val conf = new SparkConf().setAppName("SparkES test").setMaster("local[2]")
+    val conf = new SparkConf().setAppName("SparkAls test").setMaster("spark://se003:7077")
 	val sc = new SparkContext(conf)
 	val sqlContext = new SQLContext(sc)
 
-	//ElasticSearch Configurations and Input Path to the File.
-	val jsonPath = "file:///usr/hadoop/lijun/data/meta_Amazon_Instant_Video.json"
-	val esConfig = Map(("es.nodes", "se003"), ("es.port", "9200"), ("es.index.auto.create", "true"), ("es.http.timeout", "5m"))
-
-	val jsonDf = sqlContext.read.json(jsonPath)
-
-    //Storing it in Elastic Search Dynamically
-	
-	jsonDf.saveToEs("lijun/meta", cfg = esConfig)
+	val filePath = "/jli/ratings_Musical_Instruments.csv"
+	val rawData = sc.textFile(filePath)
+	val rawRating = rawData.map(_.split(",")).take(3)
+	val ratings = rawRating.map{
+		case Array(user,product,rating) => Rating(user.toInt,product.toInt,rating.toDouble)
+	}
+	val model = ALS.train(ratings,50,10,0.01)
+	model.userFeatures.count.saveAsTextFile("file:///usr/hadoop/lijun/data/")
   }
 }
