9dddb47acd5e505f70d70578364a9c7c9fb5a736
李骏 <lj852487887@gmail.com>
2017-01-06 14:47:03 +0800

    update

--- spark-app/run.sh
+++ spark-app/run.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-echo "compile spark-es test"
-sbt clean package
-echo "run spark-es test local"
-
-/opt/spark/bin/spark-submit --class SparkES --jars ./elasticsearch-spark_2.10-2.2.0.jar ./target/scala-2.10/sparkes-test_2.10-1.0.jar
+echo "run spark-app on cluster"
+/opt/spark/bin/spark-submit --class SparkStreamingKafka --jars ./lib/spark-streaming-kafka_2.10-1.6.0.jar,./lib/kafka_2.10-0.8.2.1.jar,./lib/metrics-core-2.2.0.jar,./lib/zkclient-0.3.jar,./lib/elasticsearch-spark_2.10-2.2.0.jar  ./target/scala-2.10/sparkapp-_2.10-1.0.jar
 
 
 
